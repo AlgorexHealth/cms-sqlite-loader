a70f72233cdee4a3914eb5c9b241fffc538238c8
Daniel Eklund <doeklund@gmail.com>
2017-03-02 20:51:54 -0500

    cool

--- archive_and_upload.sh
+++ archive_and_upload.sh
@@ -1,4 +1,4 @@
 mv *.db sample_$1
 cd sample_$1
 gzip *.db
-aws s3 sync 
+aws s3 cp . s3://algorex-cms-synthetic/sample_$1 --recursive --exclude "*" --include "*.db.gz"
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 # nothing here for now
 source download.sh $1
 python cms_sqlite_loader.py $1
-source archive_and_upload.sh
+source archive_and_upload.sh $1
